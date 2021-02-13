fbea847ce99cb54d7379e6ae7adb5c7f519480b9
Edouard <edouardkeyser@gmail.com>
2021-02-13 11:07:06 +0100

    first comit

---
+++ LePendu/Pendu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LePendu
+{
+    public struct Pendu
+    {
+        string motMystere;
+        public string motTrouve;
+        public int pointVie;
+        List<char> propositionCorrect;
+        List<char> propositionIncorrect;
+        public bool Fin;
+
+        public void Init()
+        {
+            string[] mots = { "elephan", "banane", "voiture", "avion" };
+            motMystere = vasChercher(mots);
+            foreach (char item in motMystere)
+            {
+                motTrouve += "_";
+            }
+            propositionCorrect = new List<char>();
+            propositionIncorrect = new List<char>();
+            pointVie = motMystere.Length;
+            Fin = false;
+        }
+        public string fnJeu(char lettre)
+        {
+            string message = "";
+
+                List<int> pos = new List<int>();
+                bool trouve = false;
+
+                for (int i = 0; i < motMystere.Length; i++)
+                {
+                    char lettreCourante = motMystere[i];
+                    if (lettre.ToString().ToLower() == lettreCourante.ToString().ToLower())
+                    {
+                        trouve = true;
+                        pos.Add(i);
+                    }
+                }
+                if (trouve == true)
+                {
+                    propositionCorrect.Add(lettre);
+                    string tempMotTrouve = "";
+                    for (int i = 0; i < motMystere.Length; i++)
+                    {
+                        if (!pos.Contains(i)) tempMotTrouve += motTrouve[i];
+                        else tempMotTrouve += lettre;
+                    }
+                    motTrouve = tempMotTrouve;
+                    message = $"la lettre \"{lettre}\" a été rouvée";
+                }
+                else
+                {
+                    pointVie--;
+                    propositionIncorrect.Add(lettre);
+                    message = $"la lettre \"{lettre}\" n'a pas été rouvée";
+                }
+
+            Fin = motMystere.ToLower() == motTrouve.ToLower() || !(pointVie > 0);
+            return message;
+        }
+
+        public string getStatus()
+        {
+            return $"==> Le mot actuel est {motTrouve} \r\n==> " +
+                $"Les points de vie sont : {pointVie} \r\n================";
+        }
+
+
+        string vasChercher(string[] mots)
+        {
+            Random rnd = new Random();
+            int pos = rnd.Next(0, mots.Length);
+            return mots[pos];
+        }
+    
+    }
+
+
+
+}
---
+++ LePendu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LePendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pendu pendu = new Pendu();
+            pendu.Init();
+
+            while (!pendu.Fin)
+            {
+                //demander les info => prg
+                Console.WriteLine("Entrez une lettre");
+                char lettre = char.Parse(Console.ReadLine());
+                //vérifier la lettre => struct
+                string message = pendu.fnJeu(lettre);
+
+                //Afficher les info
+                Console.WriteLine(message);
+                Console.WriteLine(pendu.getStatus());
+            }
+            if (pendu.pointVie == 0) Console.WriteLine("Désolé perdu");
+            else Console.WriteLine("Bravo !");
+        }
+    }
+}
